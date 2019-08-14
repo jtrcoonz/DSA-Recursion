d23580a751fb68a045443c8fb7a1fffa4cd46a27
Jacob Coons <jacobtcoons@gmail.com>
2019-08-14 14:40:00 -0700

    counting sheep DONE

---
+++ counting-sheep.js
@@ -0,0 +1,11 @@
+const countingSheep = function(num) {
+    if (num === 0) {
+      console.log("All sheep jumped over the fence");
+      return;
+    }
+  
+    console.log(num + ": Another sheep jumped over the fence");
+    countingSheep(num - 1);
+  }
+  
+  countingSheep(4);
