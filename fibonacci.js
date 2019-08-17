bc3eff3cc7b7d5f0d66ba8cc94c0faa0a60cc37d
Jacob Coons <jacobtcoons@gmail.com>
2019-08-16 19:31:57 -0700

    edited fibonacci code to account for the possibility of negative numbers

--- fibonacci.js
+++ fibonacci.js
@@ -1,8 +1,11 @@
 const fibonacci = function(n) {
-    if (n < 2) {
-      return n;
-    }
-    return (fibonacci(n - 1) + fibonacci(n - 2));
+  if (n < 0) {
+    return 'error: fibonacci sequence only works with positive integers';
+  }
+  if (n < 2) {
+    return n;
+  }
+  return (fibonacci(n - 1) + fibonacci(n - 2));
 }
-  
+
 fibonacci(10);
